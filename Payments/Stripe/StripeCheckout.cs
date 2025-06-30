f07372f1de5eafd5da5d282736dd76afd00f70b0
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2025-06-30 15:02:07 -0400

    chore: refactor

---
+++ Payments/Stripe/StripeCheckout.cs
@@ -0,0 +1,42 @@
+#!/usr/bin/env -S dotnet run
+#:sdk Microsoft.NET.Sdk.Web
+#:package Stripe.net@48.2.0
+
+using System;
+using Stripe;
+using Stripe.Checkout;
+
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.MapPost("/checkout", async () => {
+
+    StripeConfiguration.ApiKey = Environment.GetEnvironmentVariable("STRIPE_SECRET_KEY");
+    var options = new SessionCreateOptions
+    {
+        SuccessUrl = "http://localhost:5000/success",
+        CancelUrl = "http://localhost:5000/cancel",
+        PaymentMethodTypes = [ "card" ],
+        Mode = "payment",
+        LineItems = [
+            new SessionLineItemOptions
+            {
+                Price = "price_1RWcneDbbyfyaE6FWCPr6lWQ",
+                Quantity = 1,
+            }
+        ]
+    };
+    try
+    {
+        var service = new SessionService();
+        var session = await service.CreateAsync(options);
+        return Results.Redirect(session.Url);
+    }
+    catch (StripeException e)
+    {
+        return Results.BadRequest(e.Message);
+    }
+});
+app.MapGet("/success", () => "Success!");
+app.MapGet("/cancel", () => "Cancel!");
+app.Run();
