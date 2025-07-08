75b5fed556818997a473fefe1ae222cbf52a99d7
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2025-07-08 14:12:07 -0400

    feat: add global exception handler

---
+++ WebAPI/MinimalApi/GlobalExceptionHandler.cs
@@ -0,0 +1,41 @@
+#!/usr/bin/env -S dotnet run
+#:sdk Microsoft.NET.Sdk.Web
+
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+// Register the global exception handler
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+var app = builder.Build();
+
+// Use the global exception handler
+app.UseExceptionHandler();
+app.MapGet("/", () => {
+	throw new Exception("This is an unhandled exception in /");
+	return "hello world";
+});
+app.Run();
+
+
+public sealed class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+		var problemDetails = new ProblemDetails
+        {
+            Title = "An error occurred",
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.Message
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
