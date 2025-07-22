6e07e5081c032d4e64727bc4fe03d9cebc767489
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2025-07-22 13:10:48 -0400

    feat: add more test cases for tunit

--- Testing/TUnitBasic.cs
+++ Testing/TUnitBasic.cs
@@ -17,6 +17,17 @@ public class MyTestClass
         await Assert.That(result).IsEqualTo(3);
     }
 
+	[Test]
+	[Arguments(1, 1, 2)]
+	[Arguments(2, 3, 5)]
+	public async Task MyTestWithParameter(int a, int b, int expected) 
+	{
+        var result = Add(a, b);
+
+        await Assert.That(result).IsEqualTo(expected);
+	}
+
+
     private int Add(int x, int y)
     {
         return x + y;
