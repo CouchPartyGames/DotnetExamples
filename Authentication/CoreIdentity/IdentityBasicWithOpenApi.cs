6d39cb858d84bbea40a817f2bf14afa965082563
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2025-07-26 20:58:41 -0400

    fix: correct identity core w/ memory

--- Authentication/CoreIdentity/IdentityBasicWithOpenApi.cs
+++ Authentication/CoreIdentity/IdentityBasicWithOpenApi.cs
@@ -2,35 +2,50 @@
 #:sdk Microsoft.NET.Sdk.Web
 #:package Microsoft.EntityFrameworkCore.InMemory@9.0.*
 #:package Microsoft.EntityFrameworkCore.Tools@9.0.*
+#:package Microsoft.AspNetCore.Identity@2.3.*
 #:package Microsoft.AspNetCore.Identity.EntityFrameworkCore@9.0.*
 #:package Microsoft.AspNetCore.OpenApi@9.0.*
 #:package Scalar.AspNetCore@2.4.22
 #:property PublishAot=false     
 // Must disable AOT
 
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder();
-builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
+builder.Services.AddAuthentication().AddCookie("Identity.Bearer");
 builder.Services.AddAuthorization();
-builder.Services.AddIdentityCore<User>()
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddApiEndpoints();
+// Add Database Dependency
 builder.Services.AddDbContext<AppDbContext>(opts =>
 { 
     opts.UseInMemoryDatabase("MyTestDatabase");
 });
+// Add 
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
+
+builder.Services.ConfigureApplicationCookie(opts => {
+	opts.AccessDeniedPath = "/account/denied";
+});
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+/*
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await context.Database.EnsureCreatedAsync();
+}*/
 
     // Authentication Middleware
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapIdentityApi<User>();
+app.MapIdentityApi<IdentityUser>();
 app.MapOpenApi();
 app.MapScalarApiReference();
 
@@ -39,20 +54,8 @@ app.MapGet("/", () => Results.Redirect("/scalar"))
 
 app.Run();
 
-
-public sealed class User : IdentityUser
-{
-    public string? Initials { get; set; }
-}
-
-public sealed class AppDbContext : DbContext
+public sealed class AppDbContext : IdentityDbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts) {}
 
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-
-        builder.Entity<User>().Property(u => u.Initials).HasMaxLength(2);
-    }
 }
