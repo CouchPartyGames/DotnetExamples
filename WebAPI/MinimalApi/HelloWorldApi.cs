9249ffcd1d4f2d5e6c186927a3bb94c59cb208d9
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2025-08-16 21:24:02 -0400

    fix: better min api examples

--- WebAPI/MinimalApi/HelloWorldApi.cs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env dotnet
-#:sdk Microsoft.NET.Sdk.Web
-
-// Very Simple Minimal API
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-app.MapGet("/", () => "Hello World");
-app.Run();
--- WebAPI/MinimalApi/MinApiBasic.cs
+++ WebAPI/MinimalApi/MinApiBasic.cs
@@ -2,12 +2,16 @@
 #:sdk Microsoft.NET.Sdk.Web
 
 // Very Simple Minimal API
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World");
 app.MapGet("/local", LocalFunction);
 app.MapGet("/delegate", EndpointExtensions.AnotherHelloWorld);
+app.MapGet("/strongly-typed", EndpointExtensions.StronglyTyped);
 
 app.Run();
 
@@ -16,4 +20,9 @@ string LocalFunction() => "This is local function";
 public static class EndpointExtensions
 {
     public static string AnotherHelloWorld() => "Hello World";
+
+    public static Results<Ok<string>, BadRequest> StronglyTyped()
+    {
+        return TypedResults.Ok("Strongly Typed Example");
+    }
 }
