53c4258b772caa48db7aedc7912bb0db619f48ee
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2025-08-20 15:43:09 -0400

    chore: remove old stuff

--- WebAPI/CORS.cs
+++ WebAPI/CORS.cs
@@ -1,6 +1,47 @@
-namespace DefaultNamespace;
+#!/usr/bin/env dotnet
+#:sdk Microsoft.NET.Sdk.Web
 
-public class CORS
+// Simple example of using CORS middleware
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Step - Register Cors Policies
+//  https://learn.microsoft.com/en-us/dotnet/api/microsoft.extensions.dependencyinjection.corsservicecollectionextensions.addcors?view=aspnetcore-9.0
+builder.Services.AddCors(opts =>
 {
+    opts.AddPolicy(CorsPolicyExtensions.ProductionPolicyName, 
+        CorsPolicyExtensions.ProductionPolicy());
     
+    opts.AddPolicy("LocalhostOpen", corsOpts =>
+    {
+        corsOpts
+            .WithOrigins("http://localhost:5000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+var app = builder.Build();
+
+// Step - Apply Specific Cors Middleware
+//  Order is important, UseCors() must be called before UseResponseCache()
+app.UseCors(CorsPolicyExtensions.ProductionPolicyName);
+
+app.MapGet("/", () => "Hello World");
+app.Run();
+
+// A Better approach to defining CORS Policy Names and Policies
+// This allows for easier Testing of CORS policies 
+public static class CorsPolicyExtensions
+{
+    public const string ProductionPolicyName = "Production";
+    
+    public static CorsPolicy ProductionPolicy() => 
+        new CorsPolicyBuilder()
+            .WithOrigins("https://my.site")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .Build();
 }
