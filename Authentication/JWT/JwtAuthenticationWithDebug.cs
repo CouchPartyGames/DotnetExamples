20e836807b31f96f569662de44289cbb5336946b
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2025-08-26 08:15:41 -0400

    chore: add exceptions

--- Authentication/JWT/JwtAuthenticationWithDebug.cs
+++ Authentication/JWT/JwtAuthenticationWithDebug.cs
@@ -1,6 +1,51 @@
-namespace DefaultNamespace;
+#!/usr/bin/env dotnet
+#:sdk Microsoft.NET.Sdk.Web
+#:package Microsoft.AspNetCore.Authentication.JwtBearer@10.0.0-preview.*
 
-public class JwtAuthenticationWithDebug
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication()
+    .AddJwtBearer(opts =>
+    {
+            // Step - Enable More Detailed error message
+            // This should be turned off in production
+        opts.IncludeErrorDetails = true;
+    });
+
+var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapGet("/", () =>
 {
-    
-}
+    return Results.Content("<h1>Hello World!</h1><a href=/login>Login</a>&nbsp;<a href=/protected>Protected Page</a>",
+        "text/html");
+});
+app.MapGet("/login", async (HttpContext ctx) =>
+{
+    await ctx.SignInAsync(new ClaimsPrincipal(new []
+    {
+        new ClaimsIdentity([
+            new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Email, "test@test.com"),
+        ], CertificateJwtBearerDefaults.AuthenticationScheme)
+    }));
+    return Results.Text("You have logged in and received a cookie");
+});
+app.MapGet("/signout", await (HttpContext ctx) =>
+{
+    // Right now, this pointless
+    // In future, you could implement a blacklist to avoid a signed out jwt from being reused
+});
+app.MapGet("/protected", () => "Secret")
+    .RequireAuthorization();
+app.MapGet("/user", (ClaimsPrincipal principal) =>
+{
+    var claims = principal.Claims.ToDictionary(c => c.Type, c => c.Value);
+    return Results.Ok(claims);
+}).RequireAuthorization();
+
+app.Run();
